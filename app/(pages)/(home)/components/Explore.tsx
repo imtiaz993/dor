9f76cd284405c71d8302be4d087697e0d0d60f74
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2024-10-13 13:31:21 +0500

    build fix v2

--- app/(pages)/(home)/components/Explore.tsx
+++ app/(pages)/(home)/components/Explore.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import React, { useRef, useState } from "react";
 import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Explore = () => {
   const swiperRef = useRef<any>();
--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,12 +1,14 @@
+import dynamic from "next/dynamic";
 import Navbar from "@/app/components/Navbar";
 import Hero from "./components/Hero";
-import Explore from "./components/Explore";
 import MultiSliders from "./components/MultiSliders";
 import MoviesSlider from "./components/MoviesSlider";
 import Subscription from "./components/Subscription";
 import Features from "./components/Features";
 import Faqs from "./components/Faqs";
 import Footer from "@/app/components/Footer";
+
+const Explore = dynamic(() => import("./components/Explore"), { ssr: false });
 
 export default function Home() {
   return (
